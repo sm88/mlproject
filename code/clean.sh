b03f68bb4ebc590350d5660b7918abdec78ed157
Sushant Mahajan <smahajan@cse.iitb.ac.in>
2016-03-21 04:05:12 +0530

    added initial dataset and cleanup file

---
+++ code/clean.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+if [[ $# -ne 1 ]]; then
+	echo "usage ./clean.sh filename"
+	exit 1
+fi
+
+if ! [[ -e $1 ]]; then
+	echo "invalid file"
+	exit 1
+fi
+
+#clean
+echo "cleaning..."
+sed -i -e 's#[ ]*\[[ ]*##g' -e 's#[ ]*\][ ]*##g' -e 's/^[0-9]\+---\([a-zA-Z]\+\)/\1#/g' -e "s/[\",;'\(\)-]//g" -e 's/[ ]\{2,\}/ /g' -e 's/[ ]*[.!\?][ ]*/. /g' $1
+#sed -i -e 's#[ ]*\[[ ]*##g' $1 
+#sed -i -e 's#[ ]*\][ ]*##g' $1 
+#sed -i -e 's/[0-9]\+---//g' $1 
+#sed -i -e 's/---/,/g' $1
+echo "done"
+
