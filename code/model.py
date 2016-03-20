ebe8b4c8e924f9bf26a41be5f7a3da8f2dd672ab
Sushant Mahajan <smahajan@cse.iitb.ac.in>
2016-03-21 05:19:37 +0530

    started adding documentation, took care if period in documents

--- code/model.py
+++ code/model.py
@@ -4,13 +4,29 @@ import sys
 import csv
 import pprint as pp
 
+##Global variables
 datPath = '../data'
 paths = {'stop':datPath+'/stopwords.txt','data':datPath+'/data.txt'}
+#set containing all stop words (words not informative about query)
 stopSet = set()
+#map emotions we are not modelling to ones we are
 emotionMap = {'joy':'joy', 'fear':'anger', 'anger':'anger', 'disgust':'anger', 'sadness':'sadness', 'shame':'sadness', 'guilt':'sadness'}
+#emotions being modelled
 primaryEmotion = ['joy','anger','sadness']
 
 def init(toLower=True):
+    """
+    Function to fill up some important global variables.
+    Should be the first to be called.
+
+    Keyword arguments:
+    toLower -- default argument, specifying whether to convert string to lowercase before processing.
+
+    Return:
+    testX -- list of all documents 
+    testY -- corresponding emotions
+    """ 
+
     global stopSet,paths
     (testX,testY) = ([],[])
     with open(paths["stop"]) as sFile:
@@ -38,11 +54,11 @@ def removeStopWords(ls):
         validLine = []
         for word in ls[i].split():
             if word not in stopSet:
-                validLine.append(word)
+                validLine.append(word.strip("."))
         testX[i] = " ".join(validLine)
     return testX
 
-def debug(ls,onlyLen=True):
+def _debug(ls,onlyLen=True):
     for row in ls:
         if onlyLen:
             for item in row:
@@ -52,7 +68,12 @@ def debug(ls,onlyLen=True):
             for row in ls:
                 pp.pprint(row)
 
+
+
 if __name__ == "__main__":
     (testX,testY) = init()
     ctestX = removeStopWords(testX)
     ctestY = reduceEmotions(testY)
+    #_debug(zip(testX,ctestX),onlyLen = False)
+
+
